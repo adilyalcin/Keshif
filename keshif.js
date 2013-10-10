11814252649a194a99335c2a9ed092043daae0f0
Adil <Adil@Sahip.local>
2013-10-10 15:11:59 -0400

    Removed popup lib dep. + credits section is now internal (not loaded from a separet file) + config option to set keshif root directory.

--- keshif.js
+++ keshif.js
@@ -329,13 +329,14 @@ kshf.incrementLoadedTableCount = function(){
 
 // Sends the spreadsheet query, retrieves the result in asynch mode, prepares the data and updates visualization when all data is loaded.
 kshf.sendTableQuery = function(_kshf, q, sheet, tableCount){
+    var me = this;
     var tableName = sheet.name;
     q.send( function(response){
         if(response.isError()) {
             d3.select(".kshf.layout_infobox div.status_text span")
                 .text("Cannot load data");
             d3.select(".kshf.layout_infobox img")
-                .attr("src","img/alert.png")
+                .attr("src",me.dirRoot+"img/alert.png")
                 .style("height","40px");
             d3.select(".kshf.layout_infobox div.status_text div")
                 .text("("+response.getMessage()+")");
@@ -440,9 +441,10 @@ function capitaliseFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-kshf.list = function(config, root){
+kshf.list = function(_kshf, config, root){
     var i;
     var me = this;
+    this.parentKshf = _kshf;
     this.dragSrcEl = null;
     this.dom = {};
 //	this.dragIcon = document.createElement('img');
@@ -468,7 +470,7 @@ kshf.list = function(config, root){
     if(this.hideTextSearch!==true){    
         var listHeaderTopRowTextSearch = listHeaderTopRow.append("span").attr("class","bigTextSearch_wrap");
         listHeaderTopRowTextSearch.append("img")
-            .attr('src',"img/search-logo.svg")
+            .attr('src',this.parentKshf.dirRoot+"img/search-logo.svg")
             .attr("width","13")
             .style("margin-left","20px")
             ;
@@ -538,20 +540,12 @@ kshf.list = function(config, root){
         .append("div")
         .attr("title","Show Info & Credits")
         .attr("class","credits")
+        .on("click",function(){
+            me.parentKshf.root.select(".layout_infobox").style("display","block");
+            me.parentKshf.root.select("div.infobox_credit").style("display","block");
+        })
         .text("i");
 
-    $(infoCred[0][0]).magnificPopup({
-        items: {
-            src: 'keshif_credits.html',
-            type: 'ajax'
-        },
-        // Delay in milliseconds before popup is removed
-        removalDelay: 300,
-        
-        // Class that is added to popup wrapper and background
-        // make it unique to apply your CSS animations just to this exact popup
-        mainClass: 'mfp-3d-unfold'
-    });
     // TODO: implement popup for file-based resources
     listHeaderTopRow
       .append("a")
@@ -561,7 +555,7 @@ kshf.list = function(config, root){
       .append("img")
         .attr("class","datasource")
         .attr("title","Open Data Source")
-        .attr("src","img/datasource.png")
+        .attr("src",this.parentKshf.dirRoot+"img/datasource.png")
         ;
 
     var listColumnRow=listHeader.append("div").attr("class","listColumnRow");
@@ -850,6 +844,76 @@ kshf.init = function (options) {
             me.clearAllFilters();
         }
     });
+
+    this.dirRoot = "./";
+    if(options.dirRoot !== undefined){
+        this.dirRoot = options.dirRoot;
+    }
+
+    var creditString="";
+    creditString += "<div class=\"all-the-credits\">";
+    creditString += "";
+    creditString += "<div align=\"center\">";
+    creditString += "This browser is created by <span class=\"libName\">Keshif<\/span> library.";
+    creditString += "<\/br>";
+    creditString += " <div style=\"width:500px; text-align:left\" align=\"left\" class=\"boxinbox\">";
+    creditString += " <ul class=\"credits_features\">";
+    creditString += "     <li>Left: categories. Top: time. All: in synch.<\/li>";
+    creditString += "     <li>Mix and match your search. \"And\" selection across different filters, \"or\" inside same filter with multiple categories.<\/li>";
+    creditString += "     <li>Text search: Look up all items in the collections, or filter categories.<\/li>";
+    creditString += "     <li>Time filtering: Drag &amp; drop handles, set your range.<\/li>";
+    creditString += " <\/ul>";
+    creditString += " <\/div>";
+    creditString += "<\/div><br>";
+    creditString += "";
+    creditString += "<div align=\"center\" class=\"boxinbox\" style=\"font-size:0.9em\">";
+    creditString += "Get keshif source code from <a href=\"http:\/\/www.github.com\/adilyalcin\/Keshif\" target=\"_blank\"><img alt=\"github\" src=\"img\/gitHub.png\" height=\"20\" style=\"position:relative; top:5px\"><\/a> and use it on your own page.<\/br>";
+    creditString += "<iframe src=\"http:\/\/ghbtns.com\/github-btn.html?user=adilyalcin&repo=Keshif&type=watch&count=true&size=large\" allowtransparency=\"true\" frameborder=\"0\" scrolling=\"0\" width=\"152px\" height=\"30px\"><\/iframe>";
+    creditString += "<iframe src=\"http:\/\/ghbtns.com\/github-btn.html?user=adilyalcin&repo=Keshif&type=fork&count=true&size=large\" allowtransparency=\"true\" frameborder=\"0\" scrolling=\"0\" width=\"152px\" height=\"30px\"><\/iframe>";
+    creditString += "<\/div>";
+    creditString += "";
+    creditString += "<div align=\"center\" class=\"project_3rdparty \">";
+    creditString += "<div class=\"boxinbox\">";
+    creditString += " 3rd party libraries and APIs:<br\/>";
+    creditString += " <a href=\"http:\/\/d3js.org\/\" target=\"_blank\"><img src=\""+this.dirRoot+"img\/3rdparty_01.png\" style=\"width:110px; top:0px\"><\/a>";
+    creditString += " <a href=\"http:\/\/jquery.com\" target=\"_blank\"><img src=\""+this.dirRoot+"img\/3rdparty_02.png\" style=\"width:150px; top: -15px;\"><\/a>";
+    creditString += " <a href=\"https:\/\/developers.google.com\/chart\/\" target=\"_blank\"><img src=\""+this.dirRoot+"img\/3rdparty_03.png\" style=\"width:90px\"><\/a>";
+    creditString += "<\/div>";
+    creditString += "<\/div><br>";
+    creditString += "";
+    creditString += "<div style=\"width: 450px;\" align=\"center\" class=\"boxinbox\">";
+    creditString += " <a href=\"http:\/\/www.adilyalcin.me\" target=\"_blank\"><img src=\""+this.dirRoot+"img\/credit-1_01.png\" style=\"height:100px\"><\/a>";
+    creditString += " <img src=\""+this.dirRoot+"img\/credit-1_02.png\" style=\"height:100px; padding:0px 4px 0px 4px\">";
+    creditString += " <a href=\"http:\/\/www.cs.umd.edu\/hcil\/\" target=\"_blank\"><img src=\""+this.dirRoot+"img\/credit-1_03.png\" style=\"height:100px\"><\/a>";
+    creditString += " <img src=\""+this.dirRoot+"img\/credit-1_04.png\" style=\"height:100px;padding:0px 4px 0px 4px\">";
+    creditString += " <a href=\"http:\/\/www.umd.edu\" target=\"_blank\"><img src=\""+this.dirRoot+"img\/credit-1_05.png\" style=\"height:100px\"><\/a>";
+    creditString += "<\/div><br\/><br>";
+    creditString += "";
+    creditString += "<div align=\"center\" class=\"project_fund\">";
+    creditString += "<i>Keshif<\/i> (<a target=\"_blank\" href=\"http:\/\/translate.google.com\/#auto\/en\/ke%C5%9Fif\">keşif<\/a>) means discovery and exploration in Turkish.<br\/><br\/>";
+    creditString += "Research made practical. Funded in part by <a href=\"http:\/\/www.huawei.com\">Huawei<\/a>. <\/div>";
+    creditString += "<\/div>";
+    creditString += "";
+
+    this.layout_infobox = this.root.append("div").attr("class", "kshf layout_infobox");
+    this.layout_infobox.append("div")
+        .attr("class","infobox_background")
+        .on("click",function(){
+            me.layout_infobox.style("display","none");
+        });
+    var loadingBox = this.layout_infobox.append("div").attr("class","infobox_content infobox_loading");
+    loadingBox.append("img")
+        .attr("class","status")
+        .attr("src",this.dirRoot+"img/loading.gif")
+        ;
+    var hmmm=loadingBox.append("div")
+        .attr("class","status_text");
+    hmmm.append("span")
+        .text("Loading...");
+    hmmm.append("div");
+    var creditBox = this.layout_infobox.append("div").attr("class","infobox_content infobox_credit");
+    creditBox.html(creditString);
+
     this.layoutBackground = this.root.append("div").attr("class","kshf layout_background");
     this.layoutBackground.append("div").attr("class","leftBlockBackground");
     this.layoutBackground.append("div").attr("class","leftBlockAdjustSize")
@@ -893,23 +957,11 @@ kshf.init = function (options) {
         .attr("width","13")
         .style("margin-bottom","-2px")
 //        .text("[x]")
-        .attr("src","img/Refresh_font_awesome.svg")
+        .attr("src",this.dirRoot+"img/Refresh_font_awesome.svg")
         ;
     this.layoutLeft = this.root.append("div").attr("class", "kshf layout_left");
     this.layoutRight  = this.root.append("div").attr("class", "kshf layout_right");
 	
-    this.layout_infobox = this.root.append("div").attr("class", "kshf layout_infobox");
-    this.layout_infobox.append("img")
-        .attr("class","status")
-        .attr("src","img/loading.gif")
-        ;
-    var hmmm=this.layout_infobox.append("div")
-        .attr("class","status_text");
-    hmmm.append("span")
-        .text("Loading...");
-    hmmm.append("div")
-        ;
-
     this.loadSource();
 };
 
@@ -922,8 +974,9 @@ kshf.createCharts = function(){
     this.loaded = true;
     this.updateLayout();
     this.update();
-    d3.select(".kshf.layout_infobox")
-        .style("display","none");
+    // hide infobox
+    d3.select(".kshf.layout_infobox").style("display","none");
+    d3.select("div.infobox_loading").style("display","none");
 }
 
 kshf.insertChartHeader = function(){
@@ -944,7 +997,7 @@ kshf.addBarChart = function(options){
         options.rowLabelText = function(typ){ return typ.data[1]; };
     }
     options.rowTextWidth = this.categoryTextWidth;
-    this.charts.push(new kshf.BarChart(options));
+    this.charts.push(new kshf.BarChart(this,options));
 };
 
 kshf.addRangeChart = function(options){
@@ -1003,7 +1056,7 @@ kshf.filter_AciveItems_TotalItems = function(a,b){
 };
 
 kshf.createListDisplay = function(listOptions){
-    this.listDisplay = new kshf.list(listOptions,this.layoutRight);
+    this.listDisplay = new kshf.list(this,listOptions,this.layoutRight);
 };
 
 
@@ -1243,10 +1296,11 @@ kshf.Chart.prototype.getRowLabelOffset = function(){
 // WHIZ BAR CHART
 // ***********************************************************************************************************
 
-kshf.BarChart = function(options){
+kshf.BarChart = function(_kshf, options){
     // Call the parent's constructor
     kshf.Chart.call(this, options);
     this.sortDelay = 450; // ms
+    this.parentKshf = _kshf;
 
     if(!this.options.timeItemMap){
         this.type = 'barChart';
@@ -1869,7 +1923,7 @@ kshf.BarChart.prototype.insertHeader = function(){
             ;
     if(this.showTextSearch){
         leftBlock.append("xhtml:img")
-            .attr('src',"img/search-logo.svg")
+            .attr('src',this.parentKshf.dirRoot+"img/search-logo.svg")
             .attr("class","chartRowLabelSearch")
             .attr("width","13")
             ;
