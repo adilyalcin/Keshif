db3842bd869910f3da6207c7888a254df73b048d
adil <yalcin@umd.edu>
2015-05-27 10:26:24 -0400

    Minor demo updates

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -246,6 +246,12 @@ var sendLog = function(actID, dt, ts){
 
 $(window).load(function(){
 
+    if(document.location.hostname!=="localhost"){
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-54042831-2', 'auto');
+        ga('send', 'pageview');
+    }
+
     if(showLogo){
         var keshif_logo = d3.select("body").append("a").attr("class","keshif_logo")
             .attr("href","http://www.keshif.me").attr("target","_blank");
@@ -310,6 +316,3 @@ $(window).load(function(){
 
 });
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-54042831-2', 'auto');
-ga('send', 'pageview');
