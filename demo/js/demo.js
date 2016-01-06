31ac3eadae7db29939ab1f0cf5668fbbdc3a0988
adil <yalcin@umd.edu>
2016-01-06 09:38:33 -0500

    Demos: Shared month / day of week sorting table updated + minor updates

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -159,29 +159,29 @@ var _demo = {
     6: "Saturday",
   },
   // Can be used for sorting
-  DayOfWeek_id: {
-    Sunday: 0,
-    Monday: 1,
-    Tuesday: 2,
-    Wednesday: 3,
-    Thursday: 4,
-    Friday: 5,
-    Saturday: 6,
-  },
-  Month_id: {
-    January  : 0,
-    February : 1,
-    March    : 2,
-    April    : 3,
-    May      : 4,
-    June     : 5,
-    July     : 6,
-    August   : 7,
-    September: 8,
-    October  : 9,
-    November : 10,
-    December : 11,
-  }
+  DayOfWeek_id: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ],
+  Month_id: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
 };
 
 // Based on ISO_3166 codes
